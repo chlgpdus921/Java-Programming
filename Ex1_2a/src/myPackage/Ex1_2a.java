06f977f806aba3daed043d3a0362eac58941db65
chlgpdus921 <chlgpdus921@naver.com>
2019-05-20 13:58:59 +0900

    problem

---
+++ Ex1_2a/src/myPackage/Ex1_2a.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class Ex1_2a {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int birth = sc.nextInt();
+		System.out.println(2019 - birth);
+		sc.close();
+	}
+}
